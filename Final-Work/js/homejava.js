05556b362958bcb5ea1dc893a6bc7a5aa4989b9e
Cristian Escudero <escudero89@gmail.com>
2011-11-22 21:15:29 -0300

    Cambios

--- Final-Work/js/homejava.js
+++ Final-Work/js/homejava.js
@@ -1,40 +1,42 @@
 var currentDiapositiva = -1,
-    carJson; // mi variable global json para el auto
+	carJson; // mi variable global json para el auto
 
 function cambiarDiapositiva(incremento) {
-	
+
 	$("#diapositiva").fadeOut('normal', function() {
-    
-	    if (incremento) {
-		    currentDiapositiva += 1;  
-	    } else if (currentDiapositiva > 0) {
-		    currentDiapositiva -= 1;
-	    }
 
-	    // Llamamos a car.json para que nos de una mano almacenando cosas
-	    $.getJSON('js/car.json', function(data) {
-			    // Guardamos mi carJson
-			    carJson = data;
+		if (incremento) {
+			currentDiapositiva += 1;  
+		} else if (currentDiapositiva > 0) {
+			currentDiapositiva -= 1;
+		}
 
-			    $("#diapositiva").load(carJson["orden"][currentDiapositiva], function() {
-				    if (carJson["orden"][currentDiapositiva] === "page/webgl.html") {
+		// Llamamos a car.json para que nos de una mano almacenando cosas
+		$.getJSON('js/car.json', function(data) {
+			// Guardamos mi carJson
+			carJson = data;
+
+			$("#diapositiva").load(carJson["orden"][currentDiapositiva], function() {
+				if (carJson["orden"][currentDiapositiva] === "page/webgl.html") {
 					webGLStart(); // una vez cargado, iniciamos el webGL
-				    }
-			    });
-	    });	
-	    
-	    $("#diapositiva").fadeIn('normal');
+				} else {
+					webGLStop();
+				}
+			});
+		});	
+
+		$("#diapositiva").fadeIn('normal');
 	});		
 }
 
 cambiarDiapositiva(true);
 
 $(document).keydown(function(e){
-    if (e.keyCode === 37) { 
-       cambiarDiapositiva(false);
-       return false;
-    } else if (e.keyCode === 39) {
-	cambiarDiapositiva(true);
-	return false;
-    }
+	if (e.keyCode === 37 || e.keyCode === 33) { // left key or repag
+		cambiarDiapositiva(false);
+		return false;
+	} else if (e.keyCode === 39 || e.keyCode === 34) { // right key or avpag
+		cambiarDiapositiva(true);
+		return false;
+	}
 });
--- Final-Work/js/webgl.js
+++ Final-Work/js/webgl.js
@@ -132,3 +132,7 @@ function webGLStart() {
 	// Dibujo
 	drawCarScene();
 }
+
+function webGLStop() {
+	requestAnimFrame();
+}
